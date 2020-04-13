8e85312ce2daa44ceab6cbe93b0f4fc5e51cc0e6
Georgi Pachev <georgi.pachev@itido.eu>
2020-04-13 15:37:39 +0300

    Added create ingredient feature.

--- app/recipe/tests/test_ingredient_api.py
+++ app/recipe/tests/test_ingredient_api.py
@@ -63,3 +63,20 @@ class PrivateIngredientApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """Test creating a new ingredient"""
+        payload = {'name': 'Salt'}
+        self.client.post(INGGERIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            name=payload['name'],
+            user=self.user).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test create new ingredient with invalid payload"""
+        payload = {'name': ''}
+
+        res = self.client.post(INGGERIENTS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
